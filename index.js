813d0b7704d707e1f2c5ad08fc195e63a52fc2f8
Rohithnaidu  V <rohithnaidu.v@rapyder.com>
2024-09-29 17:24:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const config = require('config')
 
 app.get('/', (req, res)=>{
-    res.send('My Sample Node.js Application')
+    res.send('Sample Node.js Application')
 })
 
 app.get('/status', (req, res)=>{
