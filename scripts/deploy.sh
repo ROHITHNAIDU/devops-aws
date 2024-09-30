cc915abc3d48be16d6663cdcc6da5c78187ea181
Rohithnaidu  V <rohithnaidu.v@rapyder.com>
2024-09-30 18:21:16 +0530

    initial commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/my-node-app
 docker rm -f my-node-app
+docker rmi -f 045291957223.dkr.ecr.ap-south-1.amazonaws.com/node-app
 docker pull 045291957223.dkr.ecr.ap-south-1.amazonaws.com/node-app:dev
 docker run -p 80:3000 --name my-node-app -d 045291957223.dkr.ecr.ap-south-1.amazonaws.com/node-app:dev
