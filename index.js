d48f5eca110abcbd13ce5a5518040209cda0247a
Rohithnaidu  V <rohithnaidu.v@rapyder.com>
2025-11-27 13:00:34 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const port = process.env.PORT || 3000;
 const config = require('config')
 
 app.get('/', (req, res)=>{
-<<<<<<< HEAD
-    res.send('Node.js Application, deployed through AWS CI/CD services')
-=======
-    res.send('Node.js Application, deployed through CI/CD Pipeline')
->>>>>>> df5c00e50bb12c7da6786032143d0d433ffb5056
+    res.send('Sample Node.js Application, deployed through CI/CD Pipeline')
 })
 
 app.get('/status', (req, res)=>{
